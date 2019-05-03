f5511f98297e74e58d8e65299dcbc453ca989f85
Vladimir <vladimir.pa@campus.technion.ac.il>
2019-05-03 04:36:55 +0300

    updated course structure

--- course.py
+++ course.py
@@ -3,7 +3,7 @@ class course:
         self.name = ""
         self.number = 0
         self.points = 0
-        self.dependencies = set()
+        self.dependencies = list()
         self.parallel = set()
         self.similarities = set()
         self.inclusive = set()
@@ -15,29 +15,29 @@ class course:
         self.number = number
 
     def set_points(self, points):
-        self.points = points
+        try:
+            self.points = int(points)
+        except ValueError:
+            self.points = 0
 
-    def add_inclusive(self, course):
-        self.inclusive.add(course)
+    def add_inclusive(self, courses):
+        self.inclusive.update(courses)
 
-    def add_dependencies(self, course):
-        self.dependencies.add(course)
+    def add_dependencies(self, courses):
+        self.dependencies.extend(courses)
 
-    def add_similarities(self, course):
-        self.similarities.add(course)
+    def add_similarities(self, courses):
+        self.similarities.update(courses)
 
-    def add_parallel(self, course):
-        self.parallel.add(course)
-
-    def set_points(self, points):
-        self.points = points
+    def add_parallel(self, courses):
+        self.parallel.update(courses)
 
     def __repr__(self):
-        repr =  "שם הקורס: {} \n" .format(self.name) \
-              + "מספר קורס: {} \n".format(self.number) \
-              + ("מס' נקודות: {} \n".format(self.points) if self.points >0 else "") \
-              + ("מקצועות קדם: {} \n".format(self.dependencies) if len(self.dependencies) > 0 else "") \
-              + ("מקצועות צמודים: {} \n".format(self.parallel) if len(self.parallel) > 0  else "") \
-              + ("מקצועות ללא זיכוי נוסף: {} \n".format(self.similarities) if len(self.similarities) > 0  else "") \
-              + ("מקצועות ללא זיכוי נוסף (מוכלים): {} \n".format(self.inclusive) if len(self.inclusive) > 0  else "")
+        repr = "שם הקורס: {} \n".format(self.name) \
+               + "מספר קורס: {} \n".format(self.number) \
+               + ("מס' נקודות: {} \n".format(self.points) if self.points > 0 else "") \
+               + ("מקצועות קדם: {} \n".format(self.dependencies) if len(self.dependencies) > 0 else "") \
+               + ("מקצועות צמודים: {} \n".format(self.parallel) if len(self.parallel) > 0 else "") \
+               + ("מקצועות ללא זיכוי נוסף: {} \n".format(self.similarities) if len(self.similarities) > 0 else "") \
+               + ("מקצועות ללא זיכוי נוסף (מוכלים): {} \n".format(self.inclusive) if len(self.inclusive) > 0 else "")
         return repr
